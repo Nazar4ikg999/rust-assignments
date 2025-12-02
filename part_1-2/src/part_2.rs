1d8df4c55221316a42f94a4ed978b0515afa4619
Nazar4ikg999 <nazar.doter228@gmail.com>
2025-12-02 22:39:09 +0200

    FFinaly done task2
    Виправив структуру Request відповідно до request.json.
    Раніше випадково натиснув Revert на GitHub, через що код пропав у PR.
    Зараз відновив файл part_2.rs і зробив правильну реалізацію згідно з вимогами до Assignment 2

--- part_1-2/src/part_2.rs
+++ part_1-2/src/part_2.rs
@@ -2,23 +2,48 @@ use serde::{Serialize, Deserialize};
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
 pub struct Request {
+    
+    #[serde(rename = "type")]
+    pub r#type: String,
+    pub stream: Stream,
+    pub gifts: Vec<Gift>,
+    pub debug: DebugInfo,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Stream {
+    pub user_id: String,
+    pub is_private: bool,
+    pub settings: u64,
+    pub shard_url: String,
+    pub public_tariff: PublicTariff,
+    pub private_tariff: PrivateTariff,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct PublicTariff {
     pub id: u64,
-    pub method: String,
-    pub endpoint: String,
-
-    pub headers: Headers,
-    pub payload: Payload,
+    pub price: u64,
+    pub duration: String,
+    pub description: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Headers {
-    pub accept: String,
-    pub authorization: String,
+pub struct PrivateTariff {
+    pub client_price: u64,
+    pub duration: String,
+    pub description: String,
 }
 
 #[derive(Debug, Serialize, Deserialize, PartialEq)]
-pub struct Payload {
-    pub active: bool,
-    pub role: String,
-    pub limit: u32,
+pub struct Gift {
+    pub id: u64,
+    pub price: u64,
+    pub description: String,
+}
+
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct DebugInfo {
+    pub duration: String,
+    pub at: String,
 }
