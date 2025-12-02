1f7d0cdad6206fbee8285e1e26dc61885e0521e3
Nazar4ikg999 <nazar.doter228@gmail.com>
2025-12-03 00:27:03 +0200

    Complete task 7
    Відформатував код, додав документацію і включив усі необхідні лінти з завдання 7. Виправив попередження Rust та Clippy. Білд проходить успішно

--- part_1-2/src/part_1.rs
+++ part_1-2/src/part_1.rs
@@ -17,82 +17,18 @@ impl Post<New> {
             title: title.into(),
             body: body.into(),
             _state: PhantomData,
-        }
-    }
-
-    
-    pub fn submit_for_moderation(self) -> Post<Unmoderated> {
-        Post {
-            title: self.title,
-            body: self.body,
-            _state: PhantomData,
-        }
+}
     }
 }
-
-impl Post<Unmoderated> {
-    
-    pub fn allow(self) -> Post<Published> {
-        Post {
-            title: self.title,
-            body: self.body,
-            _state: PhantomData,
-        }
-    }
-
-    
-    pub fn deny(self) -> Post<Deleted> {
-        Post {
-            title: self.title,
-            body: self.body,
-            _state: PhantomData,
-        }
-    }
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Headers {
+    pub accept: String,
+    pub authorization: String,
 }
 
-impl Post<Published> {
-    pub fn delete(self) -> Post<Deleted> {
-        Post {
-            title: self.title,
-            body: self.body,
-            _state: PhantomData,
-        }
-    }
-
-    pub fn title(&self) -> &str {
-        &self.title
-    }
-
-    pub fn body(&self) -> &str {
-        &self.body
-    }
-}
-
-impl Post<Deleted> {
-    pub fn title(&self) -> &str {
-        &self.title
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn happy_flow_from_new_to_published() {
-        let post = Post::new("Hello", "Rust");
-        let post = post.submit_for_moderation();
-        let post = post.allow();
-        assert_eq!(post.title(), "Hello");
-    }
-
-    #[test]
-    fn delete_published_post() {
-        let post = Post::new("Title", "Body")
-            .submit_for_moderation()
-            .allow()
-            .delete();
-        assert_eq!(post.title(), "Title");
-    }
-    
+#[derive(Debug, Serialize, Deserialize, PartialEq)]
+pub struct Payload {
+    pub active: bool,
+    pub role: String,
+    pub limit: u32,
 }
